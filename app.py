f36e7677c303356eb4c75199fe8d08a717e90bef
rodriesquivel13 <rodrigoesquivelcastillo@gmail.com>
2025-03-11 11:41:32 -0700

    cambios a styles.css

--- app.py
+++ app.py
@@ -10,6 +10,16 @@ def index():
     start_day = request.args.get('start_day', 0, type=int)  # 0: Lunes, 6: Domingo
     cal = calendar.Calendar(firstweekday=start_day)
     months = [cal.monthdayscalendar(year, i) for i in range(1, 13)]
+    
+    # Adjust the first week of January to include the last days of December
+    if months[0][0][0] == 0:
+        last_days_december = calendar.Calendar(firstweekday=start_day).monthdayscalendar(year - 1, 12)[-1]
+        last_days_december = [day for day in last_days_december if day != 0]
+        last_days_december.reverse()  # Reverse the order to start with the correct day
+        for i in range(len(months[0][0])):
+            if months[0][0][i] == 0:
+                months[0][0][i] = last_days_december.pop()
+
     day_names = [calendar.day_name[(i + start_day) % 7] for i in range(7)]
     return render_template('calendar.html', year=year, months=months, start_day=start_day, day_names=day_names, calendar=calendar)
 
