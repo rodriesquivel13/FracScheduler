28f6d84cbaacc851fc7c1021e921aaeafb7a74ed
rodriesquivel13 <rodrigoesquivelcastillo@gmail.com>
2025-07-21 10:35:24 -0700

    partición de logica a nivel frontend

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,11 @@
-from .hollydays import *
-from .parameters import *
-from . import regular
-from . import snow_sand
+from .regular.fractions import (
+    fractional_index_maker   as regular_fractional_index_maker,
+    unfractional_dates_list  as regular_unfractional_dates_list,
+    fraction_hunter          as regular_fraction_hunter,
+)
+
+from .snow_sand.fractions import (
+    fractional_index_maker   as snow_fractional_index_maker,
+    unfractional_dates_list  as snow_unfractional_dates_list,
+    fraction_hunter          as snow_fraction_hunter,
+)
