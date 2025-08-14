2175f5c606a21199e5c8daf03b79a5c5e7e3ab44
ingenieriadatostvi <resquivel@terravital.com.mx>
2025-08-14 11:24:02 -0700

    refeature: requirements fixing and app debug

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,19 @@
 from flask import Flask
 from controllers import controllers as controllers_bp
+import os
 
 def create_app():
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+    templates_dir = os.path.join(base_dir, 'frontend', 'templates')
+    static_dir = os.path.join(base_dir, 'frontend', 'static')
+
+    app = Flask(
+        __name__,
+        template_folder=templates_dir,
+        static_folder=static_dir
+    )
+
     app.register_blueprint(controllers_bp)
     return app
 
